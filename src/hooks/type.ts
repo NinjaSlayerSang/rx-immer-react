b8cfc95d9d35df5f6391d69e25a97398f1219e86
肖俊江-pn20090106 <xiaojunjiang@huize.com>
2021-11-19 16:27:59 +0800

    1. fix: change interfaces: generic type Sub; 2. up to v0.2.1

--- src/hooks/type.ts
+++ src/hooks/type.ts
@@ -1,14 +1,22 @@
-import { Plain, Sub } from 'rx-immer';
+import { Path, Plain } from 'rx-immer';
 
 import { WithUseBind } from './useBind';
 import { WithUseTwoWayBind } from './useTwoWayBind';
 import { WithUseDiachronySize } from './useDiachronySize';
 import { WithUseRoamStatus } from './useRoamStatus';
 
-export type RxImmerWithHooks<T> = Sub<
-  Plain<T> &
-    WithUseBind<T> &
-    WithUseTwoWayBind &
-    Partial<WithUseRoamStatus> &
-    Partial<WithUseDiachronySize>
->;
+export type PlainWithHooks<T> = Plain<T> &
+  WithUseBind<T> &
+  WithUseTwoWayBind &
+  Partial<WithUseRoamStatus> &
+  Partial<WithUseDiachronySize>;
+
+type Sub<C, S = void, R = C> = {
+  sub<T = any>(relativePath: Path): Sub<PlainWithHooks<T>, Sub<C, S, R>, R>;
+
+  sup(): S;
+
+  root(): Sub<R>;
+} & C;
+
+export type RxImmerWithHooks<T> = Sub<PlainWithHooks<T>>;
